b14fbbc8ce975e3591b0bce2946f46fb0fc38bd9
ethimad abdel <ethi@madmad.local>
2025-05-08 09:51:35 +0200

    initial commit

---
+++ Commander/CommanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommanderApp.swift
+//  Commander
+//
+//  Created by ethimad abdel on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct CommanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommanderTests/CommanderTests.swift
@@ -0,0 +1,17 @@
+//
+//  CommanderTests.swift
+//  CommanderTests
+//
+//  Created by ethimad abdel on 08/05/25.
+//
+
+import Testing
+@testable import Commander
+
+struct CommanderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
