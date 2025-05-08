473a8852d70ec6c59223acd53451c67f5a551b10
ethimad abdel <ethi@madmad.local>
2025-05-08 13:43:15 +0200

    added color extension

---
+++ Commander/ColorExtension.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+var accentCustomColor: String = "#00FF33"
+var darkColor: String = "#121212"
+var white: String = "#ffffff"
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        let scanner = Scanner(string: hex)
+        
+        if hex.hasPrefix("#") {
+            scanner.currentIndex = hex.index(after: hex.startIndex)
+        }
+        
+        var rgbValue: UInt64 = 0
+        scanner.scanHexInt64(&rgbValue)
+        
+        let red = Double((rgbValue >> 16) & 0xFF) / 255.0
+        let green = Double((rgbValue >> 8) & 0xFF) / 255.0
+        let blue = Double(rgbValue & 0xFF) / 255.0
+        
+        self.init(red: red, green: green, blue: blue)
+    }
+}
