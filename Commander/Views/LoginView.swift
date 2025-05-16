33e417cd3fbfc1ce7863c748f28fddf1eef5a33d
ethimad abdel <ethi@madmad.local>
2025-05-16 16:13:17 +0200

    LogInView (1) - added login

--- Commander/Views/LoginView.swift
+++ Commander/Views/LoginView.swift
@@ -1,125 +1,182 @@
 import SwiftUI
 
 struct LabelledDivider: View {
-
-let label: String
-let horizontalPadding: CGFloat
-let color: Color
-
-init(label: String, horizontalPadding: CGFloat = 20, color: Color = .gray) {
-    self.label = label
-    self.horizontalPadding = horizontalPadding
-    self.color = color
-}
-
-var body: some View {
-    HStack {
-        line
-        Text(label).foregroundColor(color)
-        line
+    
+    let label: String
+    let horizontalPadding: CGFloat
+    let color: Color
+    
+    init(label: String, horizontalPadding: CGFloat = 20, color: Color = .gray) {
+        self.label = label
+        self.horizontalPadding = horizontalPadding
+        self.color = color
+    }
+    
+    var body: some View {
+        HStack {
+            line
+            Text(label).foregroundColor(color)
+            line
+        }
+    }
+    
+    var line: some View {
+        VStack { Divider().background(color) }.padding(horizontalPadding)
     }
 }
 
-var line: some View {
-    VStack { Divider().background(color) }.padding(horizontalPadding)
-}
-}
 
 struct LogInView: View {
-    @State private var email: String = ""
-    @State private var password: String = ""
+    @EnvironmentObject var authViewModel: AuthtenticationViewModel
+    
+    @State private var shakeTrigger = false
+    @State private var shakeAnimation = 0.0
+    
+    @State var isSheetPresented: Bool = false
 
     var body: some View {
-        NavigationView {
+
             ZStack {
                 Color(hex: darkColor)
-                    .ignoresSafeArea()
+                    .ignoresSafeArea(.all, edges: .all)
+                    
 
                 VStack {
                     Text("Commander")
                         .customFont(.bold, size: 33, hexColor: accentCustomColor)
-
+                    
                     Spacer()
-
+                    
                     Text("Log In")
                         .customFont(.bold, size: 38, hexColor: "FFFFFF")
                         .frame(maxWidth: .infinity, alignment: .leading)
-
+                    
                     Text("Write your email and password to log in")
                         .customFont(.medium, size: 13, hexColor: "FFFFFF")
                         .opacity(0.65)
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.bottom, 20)
-
+                    
+                    // EMAIL INPUT, binding perchè non viene gestito qua
                     InputView(
-                        inputText: $email,
+                        inputText: $authViewModel.credentials.email,
                         inputName: "Email",
-                        placeholder: "Your email")
-                        .autocorrectionDisabled()
-
+                        placeholder: "Your email",
+                        showError: authViewModel.triedToLogin && authViewModel.credentials.email.isEmpty
+                    )
+                    .autocorrectionDisabled()
+                   
+                    
+                    if authViewModel.triedToLogin {
+                        if authViewModel.credentials.email.isEmpty {
+                            Text("Email Required")
+                                .font(.system(size: 16))
+                                .foregroundStyle(Color.red)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                       
+                        
+                    }
+                    
+                    // PASSWORD INPUT
                     InputView(
-                        inputText: $password,
+                        inputText: $authViewModel.credentials.password,
                         inputName: "Password",
                         placeholder: "Your password",
-                        isSecureField: true)
-                        .autocorrectionDisabled()
-                  
-
+                        isSecureField: true,
+                        showError: authViewModel.triedToLogin && authViewModel.credentials.password.isEmpty
+                    )
+                    .autocorrectionDisabled()
+                   
+                    
+                    if authViewModel.triedToLogin {
+                        if authViewModel.credentials.password.isEmpty {
+                            Text("Password Required")
+                                .font(.system(size: 16))
+                                .foregroundStyle(Color.red)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        }
+                    }
+                    
+                    
+                    
+                    // Forgot password
                     Button(action: {
-                        // Forgot password action
+                       
                     }) {
                         Text("Forgot your password?")
                             .customFont(.semibold, size: 14, hexColor: accentCustomColor)
                             .frame(maxWidth: .infinity, alignment: .trailing)
                     }
                     .padding(.bottom, 36)
-
-                    Button(action: {}) {
-                        Text("")}
-                        .customButton(
-                            typeBtn: .primary,
-                            textBtn: "Log In",
-                            icon: "",
-                            width: 350,
-                            height: 50,
-                            fontSizeBtn: 18,
-                            cornerRadius: 10
-                                        )
-
+                    
+                    // LOGIN BUTTON (modifier)
+                    Button {
+                        authViewModel.checkInput()
+                    } label: {
+                        Text("Login")
+                            .customFont(.bold, size: 18, hexColor: darkColor)
+                    }
+                    .modifier(ButtonView(typeBtn: .primary, width: 350, height: 50, cornerRadius: 8))
+                    
                     LabelledDivider(label: "or")
-
-                    Button(action: {}) {
-                        Text("")}
-                        .customButton(
-                            typeBtn: .secondary,
-                            textBtn: "Continue with Google",
-                            icon: "globe",
-                            width: 350,
-                            height: 50,
-                            fontSizeBtn: 18,
-                            cornerRadius: 10
-                        )
-
+                  
+                    
+                    // GOOGLE LOGIN BUTTON (estensione modifier)
+                    Button {
+                        print("fanculo")
+                        authViewModel.login()
+                    } label: {
+                        Text("Login with Google")
+                            .customFont(.regular, size: 18, hexColor: accentCustomColor)
+                    }
+                    .customButton(typeBtn: .tertiary, width: 350, height: 40, cornerRadius: 8)
+                    
+                    
                     Spacer()
                     Spacer()
-
+                   
                     HStack {
                         Text("Need an account?")
                             .customFont(.medium, size: 14, hexColor: "FFFFFF")
-                        Button(action: {
-                            // Navigate to Sign Up
-                        }) {
-                            Text("Sign Up")
+                        Text("Sign Up")
                                 .customFont(.semibold, size: 14, hexColor: accentCustomColor)
                         }
+                    .onTapGesture {
+                        print("sheet presented")
+                        isSheetPresented.toggle()
+                      
                     }
+                    
+                    .sheet(isPresented: $isSheetPresented) {
+                        FirstRegistrationView1()
+                            .background(Color(hex: darkColor).ignoresSafeArea()) // ← Forzato qui
+                            .presentationDetents([.medium, .large])
+                            .presentationDragIndicator(.visible)
+                    }
+
+
+             
+
+                    
+                    
+                    
+                    
+                    
+                    }
+                
+                .padding(.horizontal, 24)
                 }
-                .padding(24)
+
+            
             }
+
         }
-    }
-}
+    
+
 
 #Preview {
     LogInView()
+        .environmentObject(AuthtenticationViewModel())
 }
