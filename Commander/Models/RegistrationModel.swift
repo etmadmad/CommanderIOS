c010a90b3cb63922d6fddc17de8d07f754c8a1e1
ethimad abdel <ethi@madmad.local>
2025-05-28 23:19:26 +0200

    RegistrationModel - init (0)

---
+++ Commander/Models/RegistrationModel.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+struct UserRegistrationModel: Codable {
+    var username: String = ""
+    var password: String = ""
+    var password2: String = ""
+    var email: String =  ""
+    var first_name: String = ""
+    var last_name: String = ""
+    var date_of_birth: Date = Date()
+    var profile_image: Data? = nil
+    
+}
+
+struct UsernameTakenModel: Decodable {
+    let username_taken: Bool
+}
+
+struct EmailTakenModel: Decodable {
+    let email_taken: Bool
+}
+
+struct RegistrationResponse: Decodable {
+    var message: String = ""
+    let user: User
+}
+
+struct User: Codable {
+    let id: String
+    let username: String
+    let email: String
+    let first_name: String?
+    let last_name: String?
+    let date_of_birth: String?
+    let profile_image: String?
+}
+
