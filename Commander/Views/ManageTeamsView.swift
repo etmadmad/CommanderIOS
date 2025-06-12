99767fafc694859b5333a28e977eea8e6ea70379
etmadmad <e.abdelrahman@ied.edu>
2025-06-12 10:16:52 +0200

    ManageTeamsView - fixed drag & drop (3)
    fixed drag & drop, when i drag user on team card the user is removed from list. team card number increases by one

--- Commander/Views/ManageTeamsView.swift
+++ Commander/Views/ManageTeamsView.swift
@@ -41,95 +41,99 @@ struct StartRoomBarView: View {
 }
 
 
-
 struct ManageTeamsView: View {
-    @State private var numberItems: CGFloat = 2
+    @State private var numberItems: Int = 2
     @StateObject private var userInfoViewModel = UserInfoViewModel()
-    
+    @State private var membersPerTeam: [[String]] = [[], []]
+
     var colorsTeams: [String] = ["#F84848", "487DF8", "68B41E", "FFFF9F", "5E00CF"]
 
     var body: some View {
-        ZStack(alignment: .bottom){
+        ZStack(alignment: .bottom) {
             Color(hex: darkColor)
                 .ignoresSafeArea()
             VStack {
                 Spacer()
                 HStack {
                     Text("Teams")
-                        .font(.largeTitle)
-                        .fontWeight(.bold)
-                      //  .customFont(.bold, size: 25, hexColor: "FFFFFF")
+                        .customFont(.bold, size: 30, hexColor: white)
                     Spacer()
                     Image(systemName: "plus")
-                        .onTapGesture { numberItems += 1 }
+                        .onTapGesture {
+                            numberItems += 1
+                            membersPerTeam.append([])
+                        }
                     if numberItems > 2 {
                         Image(systemName: "minus")
-                            .onTapGesture { numberItems -= 1 }
+                            .onTapGesture {
+                                numberItems -= 1
+                                membersPerTeam.removeLast()
+                            }
                     }
                 }
                 .foregroundColor(.white)
-
             
                 ScrollView(.horizontal) {
                     HStack(spacing: 15) {
-                        ForEach(0..<Int(numberItems), id: \.self) { index in
+                        ForEach(0..<numberItems, id: \.self) { index in
                             let colorCard = colorsTeams[index % colorsTeams.count]
-                            TeamsCardview(numberTeam: 2, teamColorCard: colorCard)
+                            TeamsCardview(
+                                teamColorCard: colorCard,
+                                members: membersPerTeam[index],
+                                onMemberDropped: { member in
+                                    if !membersPerTeam[index].contains(member) {
+                                        membersPerTeam[index].append(member)
+                                    }
+
+                                    if let removeIndex = userInfoViewModel.users.firstIndex(where: { $0.username == member }) {
+                                        userInfoViewModel.users.remove(at: removeIndex)
+                                    }
+                                   
+                                    
+                                }
+                                    
+                            )
                         }
                     }
                 }
                 .scrollIndicators(.hidden)
             
-            
-
                 Text("Players")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundStyle(.white)
+                    .customFont(.bold, size: 30, hexColor: white)
                     .frame(maxWidth: .infinity, alignment: .leading)
-                                    // Lista profili
-                    if userInfoViewModel.isLoading {
-                        ProgressView()
-                    } else if let errorMessage = userInfoViewModel.errorMessage {
-                        Text(errorMessage).foregroundColor(.red)
-                    } else {
-                        ScrollView{
-                            LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4)) {
-                                ForEach(userInfoViewModel.users, id: \.username) { profile in
-                                    UserInfoView(userInfoViewModel: userInfoViewModel, profile: profile)
-                                        .onDrag {
-                                            return NSItemProvider(object: profile.username as NSString)
-                                        }
-                                    
-                                }
+                
+                if userInfoViewModel.isLoading {
+                    ProgressView()
+                    
+                } else if let errorMessage = userInfoViewModel.errorMessage {
+                    Text(errorMessage).foregroundColor(.red)
+                    
+                } else {
+                    ScrollView{
+                        LazyVGrid(columns: Array(repeating: GridItem(.flexible()), count: 4)) {
+                            ForEach(userInfoViewModel.users, id: \.username) { profile in
+                                UserInfoView(userInfoViewModel: userInfoViewModel, profile: profile)
+                                    .onDrag {
+                                        return NSItemProvider(object: profile.username as NSString)
+                                    }
                             }
                         }
-                        .scrollIndicators(.hidden)
                     }
-                    
-                
-                   
-                
+                    .scrollIndicators(.hidden)
+                }
             }
             .padding(.horizontal, 24)
             .onAppear {
                 userInfoViewModel.fetchUsers()
             }
-            
-            
             StartRoomBarView()
-           
-   
-                       
-
         }
     }
 }
 
- 
-#Preview {
-    StartRoomBarView()
-}
+//#Preview {
+//    StartRoomBarView()
+//}
 
 #Preview{
     ManageTeamsView()
