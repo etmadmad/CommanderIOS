307c1e626cb0f58ff10690b0d7f48e098618a46e
etmadmad <e.abdelrahman@ied.edu>
2025-09-12 18:55:09 +0200

    WebSocketService - added PlayerStatusEvent

--- Commander/Services/WebSocketService.swift
+++ Commander/Services/WebSocketService.swift
@@ -3,7 +3,7 @@ import Foundation
 
 struct PlayerStatusEvent: Codable {
     let type: String
-    let player_id: String
+    let player_id: String?
     let status: String?
     let username: String?
 }
@@ -11,9 +11,11 @@ struct PlayerStatusEvent: Codable {
 class WebSocketSessionManager {
     private let sessionRoomCode: String
     private var webSocketTask: URLSessionWebSocketTask?
+    private weak var viewModel: GameConfigurationViewModel?
 
-    init(sessionRoomCode: String) {
+    init(sessionRoomCode: String, viewModel: GameConfigurationViewModel) {
         self.sessionRoomCode = sessionRoomCode
+        self.viewModel = viewModel
         let url = URL(string: "ws://5.189.158.70:8000/ws/session/\(sessionRoomCode)/")!
         webSocketTask = URLSession(configuration: .default).webSocketTask(with: url)
         webSocketTask?.resume()
@@ -33,6 +35,19 @@ class WebSocketSessionManager {
                         do {
                             let event = try JSONDecoder().decode(PlayerStatusEvent.self, from: data)
                             print("Evento decodificato: \(event)")
+                            
+                            if event.type == "player_joined", let username = event.username {
+                                print("CRISTODIO: Giocatore unito: \(username)")
+                                DispatchQueue.main.async {
+                                    let newPlayer = Player(
+//                                        id: event.player_id,
+                                        username: username,
+                                        profileImageURL: nil // lo recuperiamo dopo
+                                    )
+                                    
+                                    self?.viewModel?.addPlayer(newPlayer)
+                                }
+                            }
             
                         } catch {
                             print("Errore decodifica JSON: \(error)")
@@ -56,6 +71,7 @@ class WebSocketSessionManager {
     
     func close() {
         webSocketTask?.cancel(with: .goingAway, reason: nil)
+        print("Websocket CLOSEDSDDD")
     }
 }
 
