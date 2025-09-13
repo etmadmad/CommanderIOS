41f95fa3cfcc7e6f51e409d9e56fda06ed07adb3
etmadmad <e.abdelrahman@ied.edu>
2025-09-13 17:07:36 +0200

    RoomCodeModalView - fixed navigation to matches
    fixed navigation to matches (including free for all), fixed leave session error

--- Commander/Rooms/RoomCodeModalView.swift
+++ Commander/Rooms/RoomCodeModalView.swift
@@ -3,41 +3,131 @@ import SwiftUI
 
 struct RoomCodeModalView: View {
     let roomName: String
+    let gameModeRoom: String
+    let maxPlayersRoom: Int
+    let durationRoom: Int
     let roomCode: String
-//    let descriptionRoom: String
     var onCreateTeams: () -> Void
+    var onStartGame: () -> Void
+    
+    ///AGGIUNTI
+    @State private var isNavigating = false
+    @EnvironmentObject var gameConfigVM: GameConfigurationViewModel
+    
     
     var body: some View {
-        VStack(spacing: 24) {
-            Spacer()
-            HStack{
+        ZStack{
+            Color(hex: darkColor)
+                .ignoresSafeArea()
+            
+            
+            VStack() {
+                Spacer()
                 Text(roomName)
                     .customFont(.bold, size: 24, hexColor: accentCustomColor)
-                Text("Match")
-                    .customFont(.bold, size: 24, hexColor: accentCustomColor)
-            }
-        
-            Text("Codice di accesso:")
-                .customFont(.regular, size: 20, hexColor: white)
-            
-            Text(roomCode)
-                .onAppear {
-                        print("RoomCodeModalView - Codice stanza: \(roomCode)")
+                
+                Group{
+                    InfoCardView(
+                        title: "Game Mode",
+                        value: "\(gameModeRoom)",
+                        iconName: "swordsIcon",
+                        isSFSymbol: false
+                        
+                    )
+                    
+                    InfoCardView(
+                        title: "Max players",
+                        value: "\(maxPlayersRoom)" + " players",
+                        iconName: "person.fill",
+                        isSFSymbol: true
+                        
+                        
+                    )
+                    
+                    InfoCardView(
+                        title: "Duration",
+                        value: "\(durationRoom)" + " minutes",
+                        iconName: "clock",
+                        isSFSymbol: true
+                        
+                        
+                    )
+                }
+                .frame(width: 350)
+                
+                Spacer()
+                
+                Text("Room Code:")
+                    .font(.system(size: 20, weight: .regular))
+                    .foregroundStyle(Color(hex: "AAAAAA"))
+                
+                HStack{
+                    
+                    
+                    
+                    Text(roomCode)
+                        .font(.system(size: 40, weight: .medium))
+                        .foregroundStyle(Color.white)
+                    
+                    Button(action: {
+                        UIPasteboard.general.string = roomCode
+                    }) {
+                        Image(systemName: "doc.on.doc")
+                            .foregroundColor(Color(hex: white))
+                            .font(.title2)
                     }
-                .font(.largeTitle)
-                .customFont(.bold, size: 40, hexColor: accentCustomColor)
-            
-            Button(action: {
-                onCreateTeams()
-            }) {
-                Text("Crea Squadre")
-                    .frame(maxWidth: .infinity)
+                    ShareLink(item: roomCode) {
+                        Image(systemName: "square.and.arrow.up")
+                            .foregroundColor(Color(hex: white))
+                            .font(.title2)
+                    }
+                }
+                
+                
+                Button(action: {
+//                    if gameModeRoom == "Free for All" {
+//                        isNavigating = true
+//                        onStartGame()
+//                        
+//                    }
+                    if gameModeRoom == "Free for All" {
+                        isNavigating = true
+                        // 👉 Avvia la partita lato server
+                        if let sessionId = gameConfigVM.currentSessionId {
+                            gameConfigVM.startSession(gameId: sessionId)
+                        }
+                        // 👉 Mantieni anche la tua logica di navigazione
+                        onStartGame()}
+                        else {
+                        isNavigating = true
+                        onCreateTeams()
+                    }
+                }) {
+                    Text(gameModeRoom == "Free for All" ? "Start" : "Create Teams")
+                        .frame(maxWidth: .infinity)
+                        .font(.system(size: 16, weight: .regular, design: .default))
+                        .foregroundStyle(Color(hex: darkColor))
+                }
+                .customButton(typeBtn: .primary, width: 350, height: 50, cornerRadius: 12)
+                .padding(.horizontal)
+                Spacer()
             }
-            .customButton(typeBtn: .primary, width: 130, height: 60, cornerRadius: 12)
-            .padding(.horizontal)
-            
-            Spacer()
+            .padding()
         }
-        .padding()
-    }
+        
+        
+        
+        .onDisappear{
+            if !isNavigating {
+                if let sessionId = gameConfigVM.currentSessionId,
+                   sessionId == roomCode,
+                   !gameConfigVM.isGameStarted {
+                    print("Leaving session from onDisappear in RoomCodeModalView (session not started yet)")
+                    gameConfigVM.leaveGameSession(gameId: sessionId)
+                } else {
+                    print("Not leaving session on onDisappear — either session started or sessionId mismatch.")
+                }
+            }
+            
+        }}
 }
