e899f6111ef3d62abcf544c875da25e603642e0e
etmadmad <e.abdelrahman@ied.edu>
2025-09-13 17:11:14 +0200

    CommanderApp - fixed gameConfig issue
    fixed issue: user sees the correct games

--- Commander/CommanderApp.swift
+++ Commander/CommanderApp.swift
@@ -48,7 +48,10 @@ import SwiftUI
 @main
 struct CommanderApp: App {
     @StateObject var authViewModel = AuthtenticationViewModel()
+    @StateObject var gameConfigVM = GameConfigurationViewModel()
     
+//    @State private var gameConfigVM = GameConfigurationViewModel()
+
     var body: some Scene {
         WindowGroup {
             Group {
@@ -56,6 +59,8 @@ struct CommanderApp: App {
                     if authViewModel.isOTPvalid {
                         TabBarView()
                             .environmentObject(authViewModel)
+                            .environmentObject(gameConfigVM)
+     
 //                        ProfileView()
 //                            .environmentObject(authViewModel)
                     } else {
@@ -67,6 +72,23 @@ struct CommanderApp: App {
                         .environmentObject(authViewModel)
                 }
             }
+//            .environmentObject(gameConfigVM)
+            .onChange(of: authViewModel.isOTPvalid) { otpValid in
+                if otpValid {
+                    gameConfigVM.getAllGames()
+                }}
+            /// EVITARE STATE LEAK
+//            .onChange(of: authViewModel.isLoggedIn) { loggedIn in
+//                if loggedIn {
+//                    // nuovo utente → crea nuovo VM fresco
+//                    gameConfigVM = GameConfigurationViewModel()
+//                    gameConfigVM.getAllGames()
+//                } else {
+//                    // logout → svuota
+//                    gameConfigVM = GameConfigurationViewModel()
+//                }
+//            }
+
         }
     }
 }
